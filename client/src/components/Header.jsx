03f313afc097145e2f475b074a3a2adfeff65bef
Jijo Jacob <Jijojacob988@gmail.com>
2025-01-08 10:32:31 +0530

    added more animations on all pages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { delay, motion } from "motion/react"
+import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const {user,setShowLogin} = useContext(AppContext)
+  const navigate = useNavigate()
+
+  const onClickHandler = ()=>{
+    if(user){
+      navigate('/result')
+    }else{
+      setShowLogin(true)
+    }
+
+  }
+
+
   return (
     <motion.div className='flex flex-col justify-center items-center text-center my-20'
     initial={{opacity:0.2,y:100}}
@@ -30,7 +45,7 @@ const Header = () => {
             transition={{ delay: 0.6, duration: 2}}
             >Unleash your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.</motion.p>
 
-<motion.button 
+<motion.button onClick={onClickHandler}
     className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
     whileHover={{ scale: 1.05 }}
     whileTap={{ scale: 0.95 }}

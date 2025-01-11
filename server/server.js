3f451998163e15f6a3974d76d855c54bb85ae557
Jijo Jacob <Jijojacob988@gmail.com>
2025-01-11 11:10:50 +0530

    Database connected with mongodb atlas

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
+import connectDB from './config/mongodb.js';
+
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+await connectDB()
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"));
